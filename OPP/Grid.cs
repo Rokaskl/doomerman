0688afc8274521864c446ec5ee434936bede4d4b
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2020-09-19 16:27:09 +0300

    Added methods for changing tile graphics and finished up switching images based on tile type

--- OPP/Grid.cs
+++ OPP/Grid.cs
@@ -8,8 +8,15 @@ namespace OPP
 {
     class Grid
     {
-        bool listenForGrid = true;
+        // TODO: load map sizes and other settings from server; hardcoding for prototype
 
+        private Tile[][] tiles = new Tile[15][]; // Tile matrix
+        
+        public bool listenForGrid = true;
+
+        /// <summary>
+        /// Listener for changes in grid (primary client listener for server messages)
+        /// </summary>
         public void ListenGrid()
         {
             while (listenForGrid)
@@ -17,6 +24,29 @@ namespace OPP
 
 
             }
+        }
+
+        public void UpdateGrid()
+        {
+            for(int x = 0; x < 15; x++)
+            {
+                for(int y = 0; y < 15; y++)
+                {
+                    tiles[x][y].UpdateGfx();
+                }
+            }
+        }
+
+        public void UpdateTile(int x, int y, TileEnumerator.TileTypeEnum tileType)
+        {
+            tiles[x][y].SetTileType(tileType);
+            tiles[x][y].UpdateGfx();            // Updates which image to use (maybe will make it private and do an auto update when setting type)
+        }
+
+        public void UpdateTile(int x, int y, PowerUpEnumerator.PowerUpType powerUpType)
+        {
+            tiles[x][y].SetPowerUpType(powerUpType);
+            tiles[x][y].UpdateGfx();
         }
 
     }
