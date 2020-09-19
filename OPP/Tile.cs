a5e7e9d75781c8362a54cd3fa511a59cd5b7cf5d
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2020-09-19 16:32:31 +0300

    Added methods for changing tile graphics and finished up switching images based on tile type

--- OPP/Tile.cs
+++ OPP/Tile.cs
@@ -4,14 +4,16 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OPP.PowerUpEnumerator;
 using static OPP.TileEnumerator;
 
 namespace OPP
 {
     class Tile : IDie
     {
-        public Image tileGfx = Image.FromFile("/Resources/empty.png");
-        public TileTypeEnum tileType = TileTypeEnum.Empty;
+        private Image tileGfx = Image.FromFile("/Resources/empty.png");
+        private TileTypeEnum tileType = TileTypeEnum.Empty;
+        private PowerUpType powerUpType = PowerUpType.None;
 
         public void Die()
         {
@@ -30,8 +32,8 @@ namespace OPP
                     tileGfx = Image.FromFile("/Resources/crate.png");
                     break;
                 case TileTypeEnum.Player:
-                    // by ID
-                    /*switch (Player.GetID())
+
+                    switch (ClientManager.Instance.GetPlayerID())
                     {
                         case 0:
                             tileGfx = Image.FromFile("/Resources/player1.png");
@@ -45,21 +47,70 @@ namespace OPP
                         case 3:
                             tileGfx = Image.FromFile("/Resources/player4.png");
                             break;
-                    }*/
+                    }
                     
                     break;
                 case TileTypeEnum.PowerUp:
                     // TODO
-                    tileGfx = Image.FromFile("/Resources/empty.png");
-                    break;
+                    switch (powerUpType)
+                    {
+                        case PowerUpType.IncreaseBombCount:
+                            tileGfx = Image.FromFile("/Resources/powerup_bombcount_increase.png");
+                            break;
+
+                        case PowerUpType.DecreaseBombCount:
+                            tileGfx = Image.FromFile("/Resources/powerup_bombcount_decrease.png");
+                            break;
+
+                        case PowerUpType.IncreaseBombRange:
+                            tileGfx = Image.FromFile("/Resources/powerup_bombrange_increase.png");
+                            break;
+
+                        case PowerUpType.DecreaseBombRange:
+                            tileGfx = Image.FromFile("/Resources/powerup_bombrange_decrease.png");
+                            break;
+
+                        case PowerUpType.TemporaryJump:
+                            tileGfx = Image.FromFile("/Resources/powerup_jump.png");
+                            break;
+
+                        case PowerUpType.TemporaryShield:
+                            tileGfx = Image.FromFile("/Resources/powerup_shield.png");
+                            break;
+
+                        default:
+                            tileGfx = Image.FromFile("/Resources/empty.png");
+                            break;
+                    } break;
+
                 case TileTypeEnum.Wall:
                     tileGfx = Image.FromFile("/Resources/wall.png");
                     break;
+
                 default:
                     tileGfx = Image.FromFile("/Resources/empty.png");
                     break;
             }
+        }
 
+        public void SetTileType(TileTypeEnum type)
+        {
+            if (type == TileTypeEnum.PowerUp)
+                powerUpType = PowerUpType.None;
+
+            tileType = type;
+
+        }
+
+        public void SetPowerUpType(PowerUpType type)
+        {
+            tileType = TileTypeEnum.PowerUp;
+            powerUpType = type;
+        }
+
+        public Image GetTileGfx()
+        {
+            return tileGfx;
         }
     }
 }
