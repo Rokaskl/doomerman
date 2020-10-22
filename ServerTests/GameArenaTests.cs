6740442b0da9a7adb6c0c30378ba7b1f0fe61b14
rokaskl <rokasklkl@gmail.com>
2020-10-23 00:19:23 +0300

    Observer Subject Interface

---
+++ ServerTests/GameArenaTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Server;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Tests
+{
+    [TestClass()]
+    public class GameArenaTests
+    {
+        [TestMethod()]
+        public void AddPlayerTest()
+        {
+
+        }
+    }
+}
